ea9f93e87f7514e8c4a2b5c2417f0df8b950787c
Turlough Cowman <tcowman@cognegen.com>
2023-12-08 12:57:17 +0000

    Convert image to grayscale before displaying it

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import sys
 
-
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QTextEdit, QScrollArea
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import Qt
-from PIL import Image, ImageQt
+from PIL import Image
 from PyQt6.QtGui import QFont
+from PIL.ImageQt import ImageQt
 
 image_path = r"images\sample_document.jpg"
 
 
 class ImageViewer(QWidget):
     def __init__(self):
+        self.label = None
         super().__init__()
-        self.init_ui()
 
-    def init_ui(self):
-        # Vertical layout
+    def init_ui(self, pil_image):
+        """
+        Display the image with a text box
+        :param pil_image: A PIL image, already prepared by Pillow
+        """
+
         layout = QVBoxLayout(self)
         self.label = QLabel(self)
-        # Prepare PIL version- need to display grayscale for legibility
-        pil_image = Image.open(image_path)
-        self.q_image = ImageQt.ImageQt(pil_image)
-        # Set the QPixmap
-        self.pixmap = QPixmap(image_path)
-        self.label.setPixmap(self.pixmap)
+
+        # Set the Pixmap
+        q_image = ImageQt(pil_image)
+        pixmap = QPixmap(image_path)
+        self.label.setPixmap(pixmap)
 
         # Create a scroll area and add the label to it
         scroll_area = QScrollArea()
@@ -32,23 +35,30 @@ class ImageViewer(QWidget):
         scroll_area.setWidgetResizable(True)
         layout.addWidget(scroll_area)
 
-        # Create a text edit for notes
-        self.text_edit = QTextEdit(self)
-        self.text_edit.setFixedHeight(50)
-        self.text_edit.setText('Sample text')
-        layout.addWidget(self.text_edit)
+        # Create a text edit
+        text_edit = QTextEdit(self)
+        text_edit.setFixedHeight(50)
+        text_edit.setText('Sample text')
+        layout.addWidget(text_edit)
 
         # Set a larger font size
         font = QFont()
         font.setPointSize(30)  # Set the font size, e.g., 14 points
-        self.text_edit.setFont(font)
+        text_edit.setFont(font)
 
         # Set the layout on the application's window
         self.setLayout(layout)
 
 
 if __name__ == '__main__':
+
+    # Prepare PIL version- need to display grayscale for legibility
+    image = Image.open(image_path)
+    image = image.convert('L') # Grayscale
+
+    # Launch
     app = QApplication(sys.argv)
     viewer = ImageViewer()
+    viewer.init_ui(image)
     viewer.show()
     sys.exit(app.exec())
